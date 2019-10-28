af2252fa1a72524aeb6d0fe17e10a49c3bea0a25
Jan Kuester <jkuester@uni-bremen.de>
2019-10-28 15:18:11 +0100

    implemented get and post route

--- package.js
+++ package.js
@@ -1,24 +1,24 @@
 Package.describe({
   name: 'leaonline:webapp',
-  version: '0.0.1',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Extended Meteor webapp for easier creation of routes',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'git@github.com:leaonline/webapp.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
-});
+})
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.8.1');
-  api.use('ecmascript');
-  api.mainModule('webapp.js');
-});
+Package.onUse(function (api) {
+  api.versionsFrom('1.8.1')
+  api.use([ 'ecmascript', 'webapp' ], 'server')
+  api.addFiles('webapp.js', 'server')
+})
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('leaonline:webapp');
-  api.mainModule('webapp-tests.js');
-});
+Package.onTest(function (api) {
+  api.use('ecmascript')
+  api.use('tinytest')
+  api.use('leaonline:webapp')
+  api.mainModule('webapp-tests.js')
+})
--- webapp.js
+++ webapp.js
@@ -1,5 +1,34 @@
-// Write your package code here!
+import { WebApp } from 'meteor/webapp'
+import bodyParser from 'body-parser'
 
-// Variables exported by this module can be imported by other packages and
-// applications. See webapp-tests.js for an example of importing.
-export const name = 'webapp';
+WebApp.connectHandlers.use(bodyParser.urlencoded({ extended: false }))
+
+const get = (url, handler) => {
+  WebApp.connectHandlers.use(url, function (req, res, next) {
+    if (req.method.toLowerCase() === 'get') {
+      handler.call(this, req, res, next)
+    } else {
+      next()
+    }
+  })
+}
+
+const post = function (url, handler) {
+  WebApp.connectHandlers.use(url, function (req, res, next) {
+    if (req.method.toLowerCase() === 'post') {
+      if (req.headers[ 'content-type' ] !== 'application/x-www-form-urlencoded') {
+        // Transforms requests which are POST and aren't "x-www-form-urlencoded" content type
+        // and they pass the required information as query strings
+        // console.log('Transforming a request to form-urlencoded with the query going to the body.')
+        req.headers[ 'content-type' ] = 'application/x-www-form-urlencoded'
+        req.body = Object.assign({}, req.body, req.query)
+      }
+      handler.call(this, req, res, next)
+    } else {
+      next()
+    }
+  })
+}
+
+WebApp.connectHandlers.get = get
+WebApp.connectHandlers.post = post
