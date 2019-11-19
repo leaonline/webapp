794f167861ee65bc4bdaa4aec4b1fe5e66222e12
Jan Kuester <jkuester@uni-bremen.de>
2019-11-19 13:30:50 +0100

    move transform content-type handler into own middleware connected method

--- webapp.js
+++ webapp.js
@@ -13,19 +13,24 @@ const get = (url, handler) => {
 
 const post = function (url, handler) {
   WebApp.connectHandlers.use(url, function (req, res, next) {
+    // console.log('post request', url)
+    // console.log(req.headers)
+    // console.log(req.method)
+    // console.log(req.query, req.body)
     if (req.method.toLowerCase() === 'post') {
-      if (!req.headers[ 'content-type' ]) {
+      if (!req.headers[ 'content-type' ] || req.headers[ 'content-type' ] !== 'application/json' || req.headers[ 'content-type' ] !== 'application/x-www-form-urlencoded') {
         // Transforms requests which are POST and aren't "x-www-form-urlencoded" content type
         // and they pass the required information as query strings
         // console.log('Transforming a request to form-urlencoded with the query going to the body.')
         req.headers[ 'content-type' ] = 'application/x-www-form-urlencoded'
-        req.body = Object.assign({}, req.body, req.query)
+
+        const hasBodyKeys = Object.keys(req.body).length > 0
+        req.body = hasBodyKeys ? Object.assign({}, req.query, req.body) : Object.assign({}, req.body, req.query)
       }
-      handler.call(this, req, res, next)
-    } else {
-      next()
     }
+    next()
   })
+  WebApp.connectHandlers.use(url, handler)
 }
 
 const urlEncoded = function ({ extended = false, limit = '100kb' } = {}) {
